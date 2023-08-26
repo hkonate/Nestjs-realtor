aa20d0cb19eb11a2bc843dfbdd44f770958bd1d3
hkonate <konate.tronca@gmail.com>
2023-08-26 02:50:27 +0200

    getHomes endpoint should construct filter correctly test done

--- src/home/home.controller.spec.ts
+++ src/home/home.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeController } from './home.controller';
+import { HomeService } from './home.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('HomeController', () => {
   let controller: HomeController;
+  let homeService: HomeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HomeController],
+      providers: [
+        {
+          provide: HomeService,
+          useValue: {
+            getHomes: jest.fn().mockRejectedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HomeController>(HomeController);
+    homeService = module.get<HomeService>(HomeService);
   });
+  describe('getHomes', () => {
+    it('should construct filter object correctly', async () => {
+      const mockGetHomes = jest.fn().mockReturnValue([]);
+      jest.spyOn(homeService, 'getHomes').mockImplementation(mockGetHomes);
+      await controller.getHomes('Toronto', '1500000');
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+      expect(mockGetHomes).toBeCalledWith({
+        city: 'Toronto',
+        price: {
+          gte: 1500000,
+        },
+      });
+    });
   });
 });
